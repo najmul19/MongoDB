4941c8a88a8c40588e5773abb7ae824e80c5f48a
najmul <mdnajmulislam10992@gmail.com>
2025-04-20 22:46:46 +0600

    read data find multiple user and display on ui

--- crud-server/index.js
+++ crud-server/index.js
@@ -25,12 +25,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("usersDB");
+    const userCollection = database.collection("users");
+    app.get('/users',async(req, res)=>{
+      const cursor = userCollection.find()
+      const result  = await cursor.toArray();
+      res.send(result)
+    })
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      console.log('new user', user)
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
