7d6a3c2f77dc51f42ee5ae5e3c6c117829907f96
najmul <mdnajmulislam10992@gmail.com>
2025-04-21 23:37:59 +0600

    addcoffe design

--- Projects/coffee-house-client/src/components/AddCoffee.jsx
+++ Projects/coffee-house-client/src/components/AddCoffee.jsx
@@ -1,11 +1,35 @@
-
-
 const AddCoffee = () => {
-    return (
-        <div>
-            <h2>Add Coffe</h2>
+  return (
+    <div>
+      <h2>Add Coffe</h2>
+      <form>
+        <div className="bg-[#F4F3F0] flex">
+          <div className="form-control">
+            <label placeholder="Coffee Name">
+              <span>Coffee Name</span>
+            </label> <br />
+            <div className="join">
+              <input
+                className="input input-bordered join-item"
+                placeholder="Coffee Name"
+              />
+            </div>
+          </div>
+          <div className="form-control">
+            <label placeholder="Coffee Name">
+              <span>Available Quantity</span>
+            </label> <br />
+            <div className="join">
+              <input
+                className="input input-bordered join-item"
+                placeholder="Available Quantity"
+              />
+            </div>
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
