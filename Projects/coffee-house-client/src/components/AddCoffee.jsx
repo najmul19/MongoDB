3ba8bc2f8317cd03430f254925cd03d1d42bf55e
najmul <mdnajmulislam10992@gmail.com>
2025-04-22 01:03:42 +0600

    implement sweet alert

--- Projects/coffee-house-client/src/components/AddCoffee.jsx
+++ Projects/coffee-house-client/src/components/AddCoffee.jsx
@@ -1,3 +1,5 @@
+import Swal from "sweetalert2";
+
 const AddCoffee = () => {
   const handleAddCoffee = (e) => {
     e.preventDefault();
@@ -32,6 +34,14 @@ const AddCoffee = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success",
+            text: "user added successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
       });
   };
   return (
