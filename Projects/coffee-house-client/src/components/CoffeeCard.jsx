75d9fe4f55bd26e39a614b6fb4c643f1c2d0f5f3
najmul <mdnajmulislam10992@gmail.com>
2025-04-22 01:50:07 +0600

    implement delete btn sweet Alert

--- Projects/coffee-house-client/src/components/CoffeeCard.jsx
+++ Projects/coffee-house-client/src/components/CoffeeCard.jsx
@@ -1,5 +1,29 @@
+import Swal from "sweetalert2";
+
 const CoffeeCard = ({ coffee }) => {
-  const { name, quantity, supplier, test, category, details, photo } = coffee;
+  const { _id, name, quantity, supplier, test, category, details, photo } =
+    coffee;
+  const handleDelete = (id) => {
+    // console.log(id)
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Swal.fire({
+        //   title: "Deleted!",
+        //   text: "Your file has been deleted.",
+        //   icon: "success",
+        // });
+        console.log("Delete confirm")
+      }
+    });
+  };
   return (
     <div className="card card-side bg-[#F4F3F0] shadow-xl">
       <figure>
@@ -16,7 +40,12 @@ const CoffeeCard = ({ coffee }) => {
           <div className="join join-vertical space-y-4">
             <button className="btn join-item">View</button>
             <button className="btn join-item">Eidt</button>
-            <button className="btn join-item">X</button>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn join-item bg-orange-600"
+            >
+              X
+            </button>
           </div>
         </div>
       </div>
