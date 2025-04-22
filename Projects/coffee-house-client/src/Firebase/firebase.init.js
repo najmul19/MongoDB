17db385b594f683e70ab35b7eaa0aef3127365ee
najmul <mdnajmulislam10992@gmail.com>
2025-04-22 15:39:52 +0600

    create firebase project and header

---
+++ Projects/coffee-house-client/src/Firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCat1iqJrdHCeGDOoCBy9d_s6GYBrvhinY",
+  authDomain: "coffee-house-202f1.firebaseapp.com",
+  projectId: "coffee-house-202f1",
+  storageBucket: "coffee-house-202f1.firebasestorage.app",
+  messagingSenderId: "236772343600",
+  appId: "1:236772343600:web:0b0cb4e6eaa8564d5b52f5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
