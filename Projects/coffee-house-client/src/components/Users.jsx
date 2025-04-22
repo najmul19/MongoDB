7da61e8e4bd5d0bcfed7258ea3a181d34367a363
najmul <mdnajmulislam10992@gmail.com>
2025-04-22 22:45:16 +0600

    deleted user with set state

--- Projects/coffee-house-client/src/components/Users.jsx
+++ Projects/coffee-house-client/src/components/Users.jsx
@@ -1,9 +1,40 @@
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Users = () => {
   const loadedUsers = useLoaderData();
   const [users, setUsers] = useState(loadedUsers);
+  const handleUserDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // delete from db
+        fetch(`http://localhost:5000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your user has been deleted.",
+                icon: "success",
+              });
+              const remainingUsers = users.filter((user) => user._id !== id);
+              setUsers(remainingUsers)
+            }
+          });
+      }
+    });
+  };
   return (
     <div>
       <h2>Users: {users.length}</h2>
@@ -28,9 +59,14 @@ const Users = () => {
                 <td>{user.email}</td>
                 <td>{user.createdAt}</td>
                 <td>
-                    <button className="btn ">X</button>
-                    <button className="btn ">E</button>
-                    </td>
+                  <button className="btn ">E</button>
+                  <button
+                    onClick={() => handleUserDelete(user._id)}
+                    className="btn "
+                  >
+                    X
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
